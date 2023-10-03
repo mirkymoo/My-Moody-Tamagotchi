048e0f081a04aa144d142748de3214a2393bb600
mirkymoo <vlada.cherkasskaya@gmail.com>
2023-10-02 21:33:15 -0400

    commit

--- MoodyTamagotchi.py
+++ MoodyTamagotchi.py
@@ -1,5 +1,12 @@
 import random
 
+class tamagotchi
+    boredom = 0
+    energy = 100
+    hungery = 0
+    alive = True
+
+
 tamagotchi_mood = 0
 
 while True:
@@ -7,24 +14,25 @@ while True:
 
     if check_tamagotchi == "Y":
         tamagotchi_mood = random.randint(0, 5)
-        if tamagotchi_mood == 0 or tamagotchi_mood == 1:
-            print("Your tamagotchi is happy.")
-        elif tamagotchi_mood == 2:
-            print("Your tamagotchi is sad.")
-            pet_tamagotchi = input("Would you like to pet your tamagotchi? Please type \"Y\" for yes or \"N\" for no:")
-            if pet_tamagotchi == "Y":
-                print("You pet your tamagotchi. It's feeling better now.")
-            else:
-                print("You chose not to pet your tamagotchi.")
-        elif tamagotchi_mood == 3:
-            print("Your tamagotchi is sleeping.")
-        elif tamagotchi_mood == 4 or tamagotchi_mood == 5:
-            print("Your tamagotchi is hungry.")
-            feed_tamagotchi = input("Would you like to feed your Tamagotchi? Please type \"Y\" for yes or \"N\" for no:")
-            if feed_tamagotchi == "Y":
-                print("You fed your tamagotchi. It's no longer hungry!")
-            else:
-                print("You chose not to feed your tamagotchi.")
+
+    if tamagotchi_mood == 0 or tamagotchi_mood == 1:
+        print("Your tamagotchi is happy.")
+    elif tamagotchi_mood == 2:
+        print("Your tamagotchi is sad.")
+        pet_tamagotchi = input("Would you like to pet your tamagotchi? Please type \"Y\" for yes or \"N\" for no:")
+        if pet_tamagotchi == "Y":
+            print("You pet your tamagotchi. It's feeling better now.")
+        else:
+            print("You chose not to pet your tamagotchi. Your tamagotchi died of loneliness.")
+    elif tamagotchi_mood == 3:
+        print("Your tamagotchi is sleeping.")
+    elif tamagotchi_mood == 4 or tamagotchi_mood == 5:
+        print("Your tamagotchi is hungry.")
+        feed_tamagotchi = input("Would you like to feed your Tamagotchi? Please type \"Y\" for yes or \"N\" for no:")
+        if feed_tamagotchi == "Y":
+            print("You fed your tamagotchi. It's no longer hungry!")
+        else:
+            print("You chose not to feed your tamagotchi. Your tamagotchi died of hunger.")
     else:
         break
 
